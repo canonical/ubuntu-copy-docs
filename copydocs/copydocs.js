ee1a5f8c3ad7418d6e289b563fcb4d984e6b5d57
Bartek Szopka <bartek.szopka@canonical.com>
2018-07-31 13:19:07 +0200

    Use Object.assign to simplify DOM and style code

--- copydocs/copydocs.js
+++ copydocs/copydocs.js
@@ -10,26 +10,29 @@
     var editStrip = document.createElement('div');
 
     var link = document.createElement('a');
-    link.className = "p-link--external";
-    link.href = copyDoc;
-    link.target = "_blank";
+    Object.assign(link, {
+      className: "p-link--external",
+      href: copyDoc,
+      target: "_blank"
+    });
     link.appendChild(document.createTextNode('copy document'));
 
     editStrip.appendChild(document.createTextNode('Propose changes to this page in the '));
     editStrip.appendChild(link);
 
-    editStrip.style.background = '#fad542';
-    editStrip.style.margin = '0';
-    editStrip.style.padding = '0.5rem 1rem 0.5rem 2.5rem';
-    editStrip.style.position = 'fixed';
-    editStrip.style.left = '1rem';
-    editStrip.style.bottom = '0';
-    editStrip.style.zIndex = '9999';
-    editStrip.style.borderRadius = '2px 2px 0 0';
-    editStrip.style.backgroundImage = 'url(https://assets.ubuntu.com/v1/b562c17f-edit.svg)';
-    editStrip.style.backgroundRepeat = 'no-repeat';
-    editStrip.style.backgroundSize = '16px 16px';
-    editStrip.style.backgroundPosition = '16px 50%';
+    Object.assign(editStrip.style, {
+      background: '#fad542',
+      padding: '0.5rem 1rem 0.5rem 2.5rem',
+      position: 'fixed',
+      left: '1rem',
+      bottom: '0',
+      zIndex: '9999',
+      borderRadius: '2px 2px 0 0',
+      backgroundImage: 'url(https://assets.ubuntu.com/v1/b562c17f-edit.svg)',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: '16px 16px',
+      backgroundPosition: '16px 50%'
+    })
 
     document.body.appendChild(editStrip);
   }
