0b86d03b425537b9a0ddbde70529842c787f57e4
Bartek Szopka <bartek.szopka@canonical.com>
2018-09-11 13:13:51 +0200

    Move styles to a stylesheet and hide widget in print

--- copydocs/copydocs.js
+++ copydocs/copydocs.js
@@ -8,6 +8,7 @@
 
   if (copyDoc) {
     var editStrip = document.createElement('div');
+    editStrip.className = 'copy-docs-widget';
 
     var link = document.createElement('a');
     Object.assign(link, {
@@ -19,20 +20,6 @@
 
     editStrip.appendChild(document.createTextNode('Propose changes to this page in the '));
     editStrip.appendChild(link);
-
-    Object.assign(editStrip.style, {
-      background: '#fad542',
-      padding: '0.5rem 1rem 0.5rem 2.5rem',
-      position: 'fixed',
-      left: '1rem',
-      bottom: '0',
-      zIndex: '9999',
-      borderRadius: '2px 2px 0 0',
-      backgroundImage: 'url(https://assets.ubuntu.com/v1/b562c17f-edit.svg)',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: '16px 16px',
-      backgroundPosition: '16px 50%'
-    })
 
     document.body.appendChild(editStrip);
   }
